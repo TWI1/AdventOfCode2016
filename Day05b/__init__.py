b6f9839bc4a51128c631efc575644ab6f7c88154
TWI <v.jedek@gmail.com>
2017-08-19 21:49:09 +0200

    Solution to  Day05b puzzle

--- Day05b/__init__.py
+++ Day05b/__init__.py
@@ -10,14 +10,25 @@ my_string='abbhdwsy'     #real thing
 
 def return_password(letters, num_of_digits):
     counter=0
-    password=[]
-    while(len(password)<num_of_digits):
+    unordered_password=[]
+    ordered_password=[]
+    indexes=[]
+    while(len(unordered_password)<8):
         letters_with_count=letters+str(counter)    
         letters_with_count_hashed=hashlib.md5(letters_with_count.encode()).hexdigest()
+        #we're only interested in hashes starting with five zeroes
         if letters_with_count_hashed[:5]=='00000':
-            password.append(letters_with_count_hashed[5:6])
+            #6th digit between 0..7 is a possition of the letter in password (only first found applies)
+            #7th character is the password letter itself
+            if letters_with_count_hashed[5:6] in ['0','1','2','3','4','5','6','7']:
+                if letters_with_count_hashed[5:6] not in indexes: 
+                    indexes.append(letters_with_count_hashed[5:6])                
+                    unordered_password.append(letters_with_count_hashed[6:7])                
         counter=counter+1
-    return password
+    #order letters in password according to their indexes specifying position of the letter in word
+    for i in range(8):
+        ordered_password.append(unordered_password[indexes.index(str(i))])
+    return ordered_password
                         
 # ========================================== program
 
